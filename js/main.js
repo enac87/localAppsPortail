581390b315b66f00d09e0570c768d4202c9277f9
Emaga Alain <emaga87@gmail.com>
2018-08-16 15:43:21 +0100

    Almost finished

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
+
+( function( $ ) {
+  $( document ).ready(function() {
+
+    $('#cssmenu > ul > li > a').click(function() {
+
+      if($(this).parent("li").hasClass('active')){
+        $(this).parent("li").removeClass('active');
+      } else {
+        $(this).parent("li").addClass('active');
+      }
+
+      $(this).next('ul').slideToggle("slow");
+
+    });
+  });
+} )( jQuery );
+
 $(function(){
 
-    $('.sub').click(function(event){
+    $('.has-sub > ul > li').click(function(event){
 
-        $('.main_sidebar>ul>li.selected_item').removeClass("selected_item");
+        $('.has-sub > ul > li.selected_item').removeClass("selected_item");
         $(this).addClass("selected_item");
         $('.holder').removeClass('visible');
-        $('#' + $(this).attr('data-target-container')).addClass('visible');
+        $('#' + $(this).children('a').attr('data-target-container')).addClass('visible');
     });
 
     $('#prev').click(function(){
